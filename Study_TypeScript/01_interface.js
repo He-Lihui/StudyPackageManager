79ebe191a1405fa3485bcbc13554301abaeb5c56
He Lihui <mistletoe997@163.com>
2021-01-26 17:14:46 +0800

    study

--- Study_TypeScript/01_interface.js
+++
@@ -1,8 +0,0 @@
-function greeter(person) {
-    return "Hello," + person.firstName + "  " + person.lastName;
-}
-var user = {
-    firstName: 'Jane',
-    lastName: 'User'
-};
-console.log(greeter(user));
